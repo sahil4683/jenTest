ebce0379b625596cb26f601482f43e16f9a63fa0
Sahil Patil <sahil4683@gmail.com>
2023-07-08 17:54:55 +0530

    ssssds

---
+++ src/main/java/com/example/jennextest/JennextestApplication.java
@@ -0,0 +1,13 @@
+package com.example.jennextest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JennextestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JennextestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jennextest/JennextestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jennextest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JennextestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
